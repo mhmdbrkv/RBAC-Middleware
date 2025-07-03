2b4404d7ef40f3687d74159066df4abe5a4fcf92
mohamed baraka <barakamohamed946@gmail.com>
2025-07-03 15:52:49 +0300

    update records.json file adding checkPermissions middleware

--- routes/record.route.js
+++ routes/record.route.js
@@ -3,11 +3,16 @@ const {
   getOneRecord,
 } = require("../controllers/record.controller");
 
-const { checkAuthenticated } = require("../middlewares/auth");
+const { checkAuthenticated } = require("../middlewares/auth.middleware");
+const { checkPermissions } = require("../middlewares/rbac.middleware");
 
 const express = require("express");
 const router = express.Router();
 
+router.use(checkPermissions("read_record"));
+
 router.get("/", checkAuthenticated, getRecords);
+
 router.get("/:id", checkAuthenticated, getOneRecord);
+
 module.exports = router;
