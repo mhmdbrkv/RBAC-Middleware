f101a0a9bb23371b9432bd7ade1b7e8c4fce2a59
mohamed baraka <barakamohamed946@gmail.com>
2025-07-05 12:52:30 +0300

    update rbac.middleware (checkPermissions) to accept one permission to check and handle if no role found

--- middleware/rbac.middleware.js
+++ middleware/rbac.middleware.js
@@ -1,44 +1,28 @@
 const { roles } = require("../config/roles.json");
 
-const checkRole = (roleName) => (req, res, next) => {
-  const { role } = req.body;
-  if (!role === roleName)
-    return res.status(403).json({
-      status: "fail",
+const checkPermissions = (permission) => (req, res, next) => {
+  if (!req.user) {
+    return res.status(403).render("notAuthorized", {
       message: "Not Authorized to perfom this action",
     });
+  }
+  const { role } = req.user;
+  let roleFound = roles.find((r) => r.name === role);
 
+  if (!roleFound) {
+    return res.status(403).render("notAuthorized", {
+      message: "Not Authorized to perfom this action",
+    });
+  }
+
+  if (!roleFound.permissions.includes(permission)) {
+    return res.status(403).render("notAuthorized", {
+      message: "Not Authorized to perfom this action",
+    });
+  }
   next();
 };
 
-const checkPermissions =
-  (...permissions) =>
-  (req, res, next) => {
-    if (!req.user) {
-      return res.status(403).render("notAuthorized", {
-        message: "Not Authorized to perfom this action",
-      });
-    }
-    const { role } = req.user;
-    let rolePermissions;
-    roles.forEach((r) => {
-      if (r.name === role) {
-        rolePermissions = r.permissions;
-      }
-    });
-
-    const rolePermissionsSet = new Set(rolePermissions);
-    const allExist = permissions.every((item) => rolePermissionsSet.has(item));
-
-    if (!allExist) {
-      return res.status(403).render("notAuthorized", {
-        message: "Not Authorized to perfom this action",
-      });
-    }
-    next();
-  };
-
 module.exports = {
-  checkRole,
   checkPermissions,
 };
